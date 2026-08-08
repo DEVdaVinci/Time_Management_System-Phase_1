426ee440e8c8193f988b1926858f4262fa2ad917
DEVdaVinci <Browndevinm@gmail.com>
2026-08-08 08:44:20 -0500

    Mainly focused on the initial setup of project. Created base files for backend and frontend. Also created dockerfiles.

---
+++ tms_frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react()
+    ],
+
+    server: {
+      host: '0.0.0.0',
+      port: Number(env.PORT_NUMBER) || 5173,
+      watch: {
+        usePolling: true,
+      },
+    },
+      
+  }
+})
