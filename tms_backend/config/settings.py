14debbb8d744e5cd792a67b4c9197b8476a9bef2
DEVdaVinci <Browndevinm@gmail.com>
2026-08-12 09:15:00 -0500

    Create a new app called task_management. Created django models: Category, Tag, Action Activity, Task. Updated the docker file to create a postgre SQL database. Updated settings.py in the back end to include the app and the database. Updated the environment variable files for the whole project as well as for the back end to include the relevant information for the database. Updated admin.py in the task management app to include the models that I just created. Made migrations and migrated, for the database.

--- tms_backend/config/settings.py
+++ tms_backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_management',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +75,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
+
 
 
 # Password validation
---
+++ tms_backend/task_management/models.py
@@ -0,0 +1,175 @@
+from uuid import UUID
+
+from django.db import models
+import uuid
+
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=False)
+    previous_version = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True, related_name="future_version")
+    version_number_major = models.IntegerField(default=1)
+    version_number_minor = models.IntegerField(default=0)
+    superior = models.ForeignKey("self", on_delete=models.PROTECT, related_name="subordinates")
+    superiors_additional = models.ManyToManyField("self", blank=True, symmetrical=False, related_name="subordinates_additional")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+    
+    
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=False)
+    previous_version = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True, related_name="future_version")
+    version_number_major = models.IntegerField(default=1)
+    version_number_minor = models.IntegerField(default=0)
+    superiors = models.ManyToManyField("self", blank=True, symmetrical=False, related_name="subordinates")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Action (models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=False)
+    previous_version = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True, related_name="future_version")
+    version_number_major = models.IntegerField(default=1)
+    version_number_minor = models.IntegerField(default=0)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="actions")
+    categories_additional = models.ManyToManyField(Category, blank=True, related_name="actions_additional")
+    tags = models.ManyToManyField(Tag, blank=True)
+    
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Activity(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=False)
+    previous_version = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True, related_name="future_version")
+    version_number_major = models.IntegerField(default=1)
+    version_number_minor = models.IntegerField(default=0)
+
+    actions = models.ManyToManyField(Action)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="activities")
+    categories_additional = models.ManyToManyField(Category, blank=True, related_name="activities_additional")
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+class Task(models.Model):
+    STATUS = (
+        ("NOT_STARTED", "Not Started"),
+        ("IN_PROGRESS", "In Progress"),
+        ("PAUSED", "Paused"),
+        ("BLOCKED", "Blocked"),
+        ("COMPLETED", "Completed"),
+        ("CANCELED", "Canceled"),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=False)
+    previous_version = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True, related_name="future_version")
+    version_number_major = models.IntegerField(default=1)
+    version_number_minor = models.IntegerField(default=0)
+    #user = models.OneToOneField()
+
+    activity = models.ForeignKey(Activity, on_delete=models.PROTECT)
+    additional_actions = models.ManyToManyField(Action, blank=True)
+    status = models.CharField(choices=STATUS, max_length=25, default="NOT_STARTED")
+    
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="tasks")
+    categories_additional = models.ManyToManyField(Category, blank=True, related_name="tasks_additional")
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+'''
+class classTemplate(models.Model):
+    VAR_NAME = (
+        (0, "None"),
+        (1, "Minimal"),
+        (2, "Very Low"),
+        (3, "Low"),
+
+        (4, "Low-Medium"),
+        (5, "Medium"),
+        (6, "Medium-High"),
+
+        (7, "High"),
+        (8, "Very High"),
+        (9, "Critical"),
+        (10, "Maximum"),
+    )
+    PRIORITY = (
+            (0, "None"),
+            (1, "Minimal"),
+            (2, "Very Low"),
+            (3, "Low"),
+    
+            (4, "Low-Medium"),
+            (5, "Medium"),
+            (6, "Medium-High"),
+    
+            (7, "High"),
+            (8, "Very High"),
+            (9, "Critical"),
+            (10, "Maximum"),
+        )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid7, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=False)
+    previous_version = models.ForeignKey("self", on_delete=models.PROTECT, blank=True, null=True, related_name="future_version")
+    version_number_major = models.IntegerField(default=1)
+    version_number_minor = models.IntegerField(default=0)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    categories_additional = models.ManyToManyField(Category, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+    connection_manyToOne = models.ForeignKey(className, on_delete=models.PROTECT)
+    connection_manyToMany = models.ManyToManyField(className)
+    var_name = models.CharField(choices=VAR_NAME, max_length=100)
+    priority = models.IntegerFieldField(choices=PRIORITY, max_length=100)
+    
+''' 
+    
