872854d8bcb111eb74fa7ff0d7366be9ceaaaed1
DEVdaVinci <Browndevinm@gmail.com>
2026-08-14 12:45:20 -0500

    removed superflous views

--- tms_backend/task_management/views.py
+++ tms_backend/task_management/views.py
@@ -10,30 +10,3 @@ from .models import *
 
 
 
-# Create your views here.
-def actions(request):
-
-    actions = Action.objects.all()
-
-    context = {
-        "actions": actions,
-    }
-    return JsonResponse(context)
-
-def action(request):
-    id = request.GET.get('id')
-    name = request.GET.get('name')
-
-    if id:
-        action = Action.objects.get(id=id)
-    elif name:
-        action = Action.objects.get(name=name)
-    else: 
-        action = None
-
-    context = {
-        "action": action,
-        "id": id,
-        "name": name,
-    }
-    return JsonResponse(context)
