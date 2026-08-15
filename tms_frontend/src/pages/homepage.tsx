96ad4de083ff9f9df2d13acc39ad6844387dcedd
DEVdaVinci <Browndevinm@gmail.com>
2026-08-14 19:21:11 -0500

    Started working on the front end implemented routing and page navigation. Also created a basic version of the home page and ensured routing works properly

---
+++ tms_frontend/src/pages/homepage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Homepage() {
+  return (
+    <>
+        <div>Homepage</div>
+        <a className="btn btn-secondary p-3" href="Categories/" role="button">Category</a>
+        <a className="btn btn-secondary p-3" href="Tags/" role="button">Tag</a>
+        <a className="btn btn-outline-secondary p-3" href="Tasks/" role="button">Task</a>
+        <a className="btn btn-outline-secondary p-3" href="Activities/" role="button">Activity</a>
+        <a className="btn btn-outline-secondary p-3" href="Actions/" role="button">Action</a>
+            
+        
+    </>
+  )
+}
+
